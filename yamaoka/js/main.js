dd41c4ada70eb5e0e5470835b0d04187ba099deb
5a1t0 <110302818+5a1t0@users.noreply.github.com>
2022-07-31 02:31:01 +0900

    Initial commit.
    練習含め、最初の完成

---
+++ yamaoka/js/main.js
@@ -0,0 +1,17 @@
+function animation(){
+    $('.fade-inTrigger').each(function(){
+        const position = $(this).offset().top;
+        const scroll = $(window).scrollTop();
+        const windowHeight = $(window).height();
+
+        if(scroll > position - windowHeight){
+            $(this).addClass('fade-in');
+        }else{
+            $(this).removeClass('fade-in');
+        }
+    });
+}
+
+$(window).scroll(function(){
+    animation();
+});
